219faae81efd5a7bd2bf95e1222e5493de3135ae
Guest User <student@epicodus.com>
2015-03-10 11:27:26 -0700

    Delete unecessary files.

--- controllers/DescriptionController.js
+++ controllers/DescriptionController.js
@@ -1,5 +1,4 @@
 rPG.controller('DescriptionCtrl', function DescriptionCtrl($scope, CharacterFactory, UtilitiesFactory){
-  // $scope.factory = UtilitiesFactory.findById(CharacterFactory, $stateParams.characterId)
   $scope.addDescription = function() {
     $scope.character.gender = $scope.gender;
     $scope.character.race = $scope.race;
--- service/WorldsFactory.js
+++
@@ -1,5 +0,0 @@
-rPG.factory('WorldsFactory', function WorldsFactory() {
-  var factory = {};
-  factory.worlds = [];
-  return factory;
-});
